dcb064c1e5a10be9357050c1b01f42285a526828
Esteban Luchsinger <eluchsingerm@hotmail.com>
2016-05-30 10:11:16 +0200

    Schriftgrösse wird jetzt geändert

--- scripts/font-size.js
+++ scripts/font-size.js
@@ -1,1 +1,14 @@
+window.addEventListener("load", registerFontSlider);
 
+/**
+ * Registers the events for the font slider.
+ */
+function registerFontSlider() {
+    var fontSlider = document.getElementById("font-slider");
+    var bodyTag = document.getElementsByTagName("body")[0];
+    
+    fontSlider.onchange = function () {
+        console.log("New Font Size Value: " + fontSlider.value);
+        bodyTag.style.fontSize = fontSlider.value + "pt";
+    };
+}
